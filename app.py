b12fb5f646bccde490934b7fe2949454de937cc1
LuizRojas <luizfrojas@gmail.com>
2025-03-28 22:18:22 -0300

    adicionando um banco de dados para armazenar as senhas em json

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
 from flask import Flask, jsonify
-from flask_sqlalchemy import SQLAlachemy
+from flask_sqlalchemy import SQLAlchemy
 from gerador import Gerador
 
 
 app = Flask(__name__)
+
+# banco de dados
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///senhas.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+# Modelo do banco de dados
+class Senha(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    senha = db.Column(db.String(255), nullable=False)
+
+# Criar o banco de dados
+with app.app_context():
+    db.create_all()
 
 def gerar_senha(tamanho=12):
     gdr = Gerador()
@@ -14,7 +29,18 @@ def gerar_senha_api(tamanho):
     if (tamanho < 6):
         return jsonify({"erro": "A senha deve ter pelo menos 6 caracteres."}), 400  # o usuario ta enviando uma requisicao em um formato incorreto
     senha = gerar_senha(tamanho)
-    return jsonify({"senha": senha})
+
+    # return jsonify({"senha": senha})
+    nova_senha = Senha(senha=senha)
+    db.session.add(nova_senha)
+    db.session.commit()
+
+    return jsonify({"id": nova_senha.id, "senha": senha})
+
+@app.route('/listar_senhas', methods=['GET'])
+def listar_senhas():
+    senhas = Senha.query.all()
+    return jsonify([{"id": s.id, "senha": s.senha} for s in senhas])
 
 if __name__ == '__main__':
     app.run(debug=True)
